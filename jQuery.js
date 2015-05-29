a8e2e5d9c060e72eed382396931bcc58a890bffe
juliabustos <juliabustosferro@gmail.com>
2015-05-29 20:56:07 +0200

    nueva pag de usuario más jQuery

---
+++ jQuery.js
@@ -0,0 +1,16 @@
+/*
+Autor=Julia
+Fecha=29-may-2015
+Licencia=default
+Version=Expression version is undefined on line 5, column 11 in Templates/ClientSide/javascript.js.
+Descripcion=
+*/
+
+
+        
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
